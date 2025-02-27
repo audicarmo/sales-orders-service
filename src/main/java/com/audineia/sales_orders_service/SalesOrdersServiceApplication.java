6feb842de08cecb0c5d4411f81a17981a029e5cb
Audinéia Carmo <56273265+audicarmo@users.noreply.github.com>
2025-02-27 10:40:39 -0300

    chore: add initial project structure

---
+++ src/main/java/com/audineia/sales_orders_service/SalesOrdersServiceApplication.java
@@ -0,0 +1,13 @@
+package com.audineia.sales_orders_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesOrdersServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesOrdersServiceApplication.class, args);
+	}
+
+}
